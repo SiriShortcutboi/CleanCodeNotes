d75b6dee31a33dffd032fdc8ab8c100d8c671af3
holden.anderson <holden.anderson@students.snow.edu>
2026-04-10 16:21:19 -0600

    DateTime Notes

--- NotesforCleanCode/Program.cs
+++ NotesforCleanCode/Program.cs
@@ -4,46 +4,13 @@ using System.ComponentModel;
 public class Program
 {
 	public static void Main()
-	{
-        //Mr Jensen hates comments, because his job doesnt do that
-        //just do better variable names
+    {
+     DateTime date = DateTime.Now;
+     DateTime date2 = DateTime.UtcNow;
 
-        //clean code 
-        //Naming matters
-        //"u is utah sales tax" is the wrong way to document this 
-        double u = 4.5;
-        int w = 0;
-        //comments are nice but your code should be self documenting
+     Console.Writleline(date);
 
-        //the right way to tell someone what your double is
-        double utahSalesTax = 4.5;
+     //Console.WriteLine("Today's date is " + date.To String("f"));
 
-        //pascalCase - header things, class, method global attributes, properties
-        //camelCase - variables, method parameters(string flipFlop);
-
-
-        //if you are going to comment you have to say, here is what is acceptable 
-        // Why not What 
-
-        //we have to use a special utah salestax because of policy 6401
-        double utahspecial6401policysalestax = 4.5;
-	}
-}
-
-public class CoolClass
-{
-    ////95% public 
-    /// 4.9% private 
-    /// 0.1% protected
-
-    // Extra bougie (private / field properties)
-    //field/properties
-    //constructors
-    //private methods
-    //methods
-
-    //ideally methods should be 1 job each
-    //if you have to explain what a chunk of code does
-    //make it a method and explain in the methood name
-
+    }
 }
